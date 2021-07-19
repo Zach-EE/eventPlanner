46988491abed38c569dc59c4f07d7ab2d3df6d75
Zach Holloway <zachholloway.n@gmail.com>
2021-07-19 16:35:30 -0400

    working date printout with col and row formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var dateCounter = 0;
 var container_tag = '<div class="row"><div class="col">Date</div><div class="col">Event</div><div class="col">Filler</div></div>';
 
 for(i=0; i < 7; i++){
-    container_tag += '<div class="row"><div class="col">test</div><div class="col">test</div><div class="col">test</div></div>'
+    container_tag += '<div class="row"><div class="col">'+moment().day(i).date()+'</div><div class="col">test</div><div class="col">test</div></div>'
     container_el.innerHTML = container_tag;
 }
 
