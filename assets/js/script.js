e2d253deba70a48f7b6703ef40c0b7e0ec9f710b
Zach-EE <zachholloway.n@gmail.com>
2021-07-21 15:51:37 -0400

    went in another direction set up planner html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var container_el = document.querySelector(".container");
 
 console.log(currentDay_el.textContent);
 
+//Display Todays Date info in page banner
 function todaysDate(){
     counter = setInterval(timer, 1000);
     function timer(){
@@ -10,17 +11,5 @@ function todaysDate(){
         console.log("----Loaded Current Date----");
     }
 }
-// * populate container with planner date elements
-var dateCounter = 0;
-var container_tag = '<div class="row"><h3 class="col">Date</h3><h3 class="col">Event</h3><h3 class="col">Filler</h3></div>'
-+'';
-
-for(i=0; i < 7; i++){
-    container_tag += '<div class="row"><div class="col">'+moment().day(i).date()+'</div><div class="col">placeHolder</div><div class="col">PlaceHolder</div></div>'
-    container_el.innerHTML = container_tag;
-}
-
-console.log(container_tag);
-
 todaysDate();
 
